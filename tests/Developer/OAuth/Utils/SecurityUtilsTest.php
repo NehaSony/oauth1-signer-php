ddbfd57c29075778808455892e5d5eef701a6d01
Neha Sony <neha.sony99@gmail.com>
2023-07-19 14:25:08 +0100

    Update SecurityUtilsTest.php

--- tests/Developer/OAuth/Utils/SecurityUtilsTest.php
+++ tests/Developer/OAuth/Utils/SecurityUtilsTest.php
@@ -28,7 +28,7 @@ class SecurityUtilsTest extends TestCase {
     public function testLoadPrivateKey_ShouldReturnKey() {
 
         // GIVEN
-        $keyContainerPath = './resources/test_key_container.p12';
+        $keyContainerPath = './resources/test_key.p12';
         $keyAlias = 'mykeyalias';
         $keyPassword = 'Password1';
 
